1ed90df5dfc96f0ef2c899b9163320aeffe73112
Felipe-P <52645370+Felipe-P@users.noreply.github.com>
2021-12-09 18:35:05 -0500

    Solucion Sprint 5

--- Sprint_4/src.js
+++ Sprint_4/src.js
@@ -3,22 +3,18 @@ const app = express();
 app.use(express.urlencoded({ extend: false }));
 app.use(express.json());
 
-app.post(
-    "/api/personal-finance/operations",
-    (req, res) => {
-        let  operacion = req.body;
-        operations.push(operacion)
-        res.status(200).json(operations)
-    }
-);
+app.post("/api/personal-finance/operations", (req, res) => {
+  let operacion = req.body;
+  operations.push(operacion);
+  res.status(200).json(operations);
+});
 
 app.get("/api/personal-finance/is-my-goal-achieved/:amount", (req, res) => {
-    let {amount} = req.params
-    amount = parseInt(amount);
+  let { amount } = req.params;
+  amount = parseInt(amount);
 
-    let result = amount > balance ? 'no cumple' : 'cumple';
-    return res.json(result)    
+  let result = amount > balance ? "no cumple" : "cumple";
+  return res.json(result);
 });
 
 module.exports = app;
-
